b3e283fdf82ad0385d0da289f2d385cc939c5408
Antonin Kadrmas <antonin.kadrmas@epptec.eu>
2023-03-07 17:39:59 +0100

    last commmit before android studio

--- src/app/home/new/image_picker/image-picker.component.ts
+++ src/app/home/new/image_picker/image-picker.component.ts
@@ -1,6 +1,6 @@
 import {Component, NgZone} from "@angular/core";
-const RadImagepicker = require('@nstudio/nativescript-rad-imagepicker').RadImagepicker;
-const PickerOptions = require('@nstudio/nativescript-rad-imagepicker').PickerOptions;
+import {ImageAsset} from "@nativescript/core";
+import * as imagePickerPlugin from '@nativescript/imagepicker';
 
 @Component({
   selector:'ns-image-picker',
@@ -16,13 +16,47 @@ export class ImagePickerComponent{
   constructor(private _ngZone: NgZone) {}
 
   public onSelectMultipleTap() {
-    const radImagePicker = new RadImagepicker( );
-    const options = new PickerOptions();
-    options.imageLimit=10
-    radImagePicker.pick(options).then((selectedImages) => {
-      if (selectedImages) {
-        console.log(selectedImages)
-      }
-    })
+    this.isSingleMode = false;
+
+      let imagePicker = imagePickerPlugin.create({
+      mode: 'multiple',
+    });
+    this.startSelection(imagePicker);
+  }
+
+  public onSelectSingleTap() {
+    this.isSingleMode = true;
+
+    let imagePicker = imagePickerPlugin.create({
+      mode: 'single',
+    });
+    this.startSelection(imagePicker);
+  }
+
+  private startSelection(imagePicker) {
+    imagePicker
+      .authorize()
+      .then(() => {
+        this._ngZone.run(() => {
+        });
+        return imagePicker.present();
+      })
+      .then((selection) => {
+        this._ngZone.run(() => {
+          // set the images to be loaded from the assets with optimal sizes (optimize memory usage)
+          selection.forEach((el: ImageAsset) => {
+            el.options.width = this.thumbSize
+            el.options.height = this.thumbSize
+          });
+          for(let value of selection){
+            this.imageAssets.push(value)
+            console.log(this.imageAssets.filter(x=>x==value).length)
+            if(this.imageAssets.filter(x=>JSON.stringify(x)==JSON.stringify(value)).length>1)this.imageAssets.splice(this.imageAssets.length-1,1)
+          }
+        });
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
   }
 }
