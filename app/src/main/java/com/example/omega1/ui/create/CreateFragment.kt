378b924254b4ad01ac575fc8e020a89d52f6ad7d
Antonin Kadrmas <antonin.kadrmas@epptec.eu>
2023-03-07 17:42:06 +0100

    android studio frontend init

---
+++ app/src/main/java/com/example/omega1/ui/create/CreateFragment.kt
@@ -0,0 +1,39 @@
+package com.example.omega1.ui.create
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.omega1.databinding.FragmentCreateBinding
+
+class CreateFragment : Fragment() {
+
+    private var _binding: FragmentCreateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(CreateViewModel::class.java)
+
+        _binding = FragmentCreateBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textCreate
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/omega1/ui/create/CreateViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.omega1.ui.create
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CreateViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
