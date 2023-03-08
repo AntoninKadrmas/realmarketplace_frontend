e46b17d3f2d80a0eafee7b2bda1f7182c4e1dcf4
Antonin Kadrmas <antonin.kadrmas@epptec.eu>
2023-03-08 19:46:33 +0100

    load image from http url

--- app/src/main/java/com/example/omega1/ui/create/CreateFragment.kt
+++ app/src/main/java/com/example/omega1/ui/create/CreateFragment.kt
@@ -1,33 +1,41 @@
 package com.example.omega1.ui.create
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.omega1.databinding.FragmentCreateBinding
+import kotlinx.android.synthetic.main.fragment_create.*
+
 
 class CreateFragment : Fragment() {
 
     private var _binding: FragmentCreateBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val notificationsViewModel =
             ViewModelProvider(this).get(CreateViewModel::class.java)
-
         _binding = FragmentCreateBinding.inflate(inflater, container, false)
+        val imageView: ImageView = binding.myImageView
         val root: View = binding.root
-
-        val textView: TextView = binding.textCreate
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.selectImage.setOnClickListener(){
+            val url="https://picsum.photos/300"
+            Glide.with(this)
+                .load(url)
+                .fitCenter()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(imageView)
         }
         return root
     }
---
+++ app/src/main/java/com/example/omega1/ui/create/imagepicker/ChooseImage.kt
@@ -0,0 +1,16 @@
+package com.example.omega1.ui.create.imagepicker
+
+import android.net.Uri
+class ChooseImage(
+){
+//    fun selectImages(){
+//        Matisse.from(this)
+//            .choose(MimeType.allOf())
+//            .countable(true)
+//            .maxSelectable(10)
+//            .thumbnailScale(0.85f)
+//            .imageEngine(GlideEngine())
+//            .forResult(REQUEST_CODE_CHOOSE)
+//
+//    }
+}
