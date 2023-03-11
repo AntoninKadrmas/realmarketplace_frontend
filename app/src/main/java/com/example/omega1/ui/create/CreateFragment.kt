cc8c84f2dc0696111c9e1614428d547f4ff3b371
Antonín Kadrmas <kadrmas.antonin@gmail.com>
2023-03-11 10:24:17 -0800

    repair menu update manifest

--- app/src/main/java/com/example/omega1/ui/create/CreateFragment.kt
+++ app/src/main/java/com/example/omega1/ui/create/CreateFragment.kt
@@ -25,10 +25,10 @@ class CreateFragment : Fragment() {
         _binding = FragmentCreateBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCreate
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textCreate
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
