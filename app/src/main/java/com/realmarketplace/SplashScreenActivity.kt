8b51657a01c8c41301888ca15dd8699907fdd87b
Antonin Kadrmas <antonin.kadrmas@epptec.eu>
2023-04-23 20:23:48 +0200

    remove work in progress part

--- app/src/main/java/com/realmarketplace/SplashScreenActivity.kt
+++ app/src/main/java/com/realmarketplace/SplashScreenActivity.kt
@@ -3,18 +3,11 @@ package com.realmarketplace
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import com.realmarketplace.ui.work.WorkInProgressActivity
-import com.realmarketplace.ui.work.WorkViewModel
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WorkViewModel.advertProductionEnable.observe(this, Observer {
-            if(it)startActivity(Intent(this,MainActivity::class.java))
-            else startActivity(Intent(this,WorkInProgressActivity::class.java))
-            finish()
-        })
-        WorkViewModel.checkEnableProduction()
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
